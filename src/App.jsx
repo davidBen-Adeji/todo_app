d9729ea7cc487719c3f54fef6f95e9f9c47e2b38
david ben-adeji <benadejidavid@gmail.com>
2023-11-18 11:01:18 +0100

    updated App.jsx and Main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,91 +1,11 @@
-import { useState } from "react";
-import { INITIAL_TASKS } from "./util/INITIAL_TASKS.js";
-import RoutesComponent from "./Components/RoutesComponent.jsx";
-import Header from "./Components/Header.jsx";
+import Header from './Components/Header.jsx';
 import Main from "./Components/Main.jsx";
 
 function App() {
-  const [tasks, setTasks] = useState(
-    JSON.parse(localStorage.getItem("tasks")) || INITIAL_TASKS
-  );
-  const [index, setIndex] = useState(
-    JSON.parse(localStorage.getItem("index")) || 0
-  );
-  const [bin, setBin] = useState(JSON.parse(localStorage.getItem("bin")) || []);
-
-  function addTaskHandler(task) {
-    setTasks((prevTasks) => {
-      localStorage.setItem("tasks", JSON.stringify([...prevTasks, task]));
-      return [...prevTasks, task];
-    });
-  }
-
-  function updateTaskHandler(updatedTask) {
-    setTasks((prevTasks) => {
-      const tasks = [...prevTasks];
-      tasks[index] = { ...updatedTask };
-      localStorage.setItem("tasks", JSON.stringify(tasks));
-      return tasks;
-    });
-  }
-
-  function deleteTaskHandler(task) {
-    setBin((prevBin) => {
-      localStorage.setItem("bin", JSON.stringify([...prevBin, task]));
-      return [...prevBin, task];
-    });
-
-    setTasks((prevTasks) => {
-      const updatedTasks = prevTasks.filter(
-        (prevTask) => prevTask.id !== task.id
-      );
-      localStorage.setItem("tasks", JSON.stringify(updatedTasks));
-      return updatedTasks;
-    });
-  }
-
-  function permanentDeleteHandler(task) {
-    setBin((prevBin) => {
-      const updatedBin = prevBin.filter((binItem) => binItem.id !== task.id);
-      localStorage.setItem("bin", JSON.stringify(updatedBin));
-      return updatedBin;
-    });
-  }
-
-  function restoreTaskHandler(task) {
-    setTasks((prevTasks) => {
-      localStorage.setItem("bin", JSON.stringify([...prevTasks, task]));
-      return [...prevTasks, task];
-    });
-
-    setBin((prevBin) => {
-      const updatedBin = prevBin.filter((binItem) => binItem.id !== task.id);
-      localStorage.setItem("bin", JSON.stringify(updatedBin));
-      return updatedBin;
-    });
-  }
-
-  function changeIndexHandler(index) {
-    localStorage.setItem("index", JSON.stringify(index));
-    setIndex(index);
-  }
-
   return (
     <>
       <Header />
-      <Main>
-        <RoutesComponent
-          task={tasks[index]}
-          tasks={tasks}
-          bin={bin}
-          onChangeIndex={changeIndexHandler}
-          onAddTask={addTaskHandler}
-          onDeleteTask={deleteTaskHandler}
-          onUpdateTask={updateTaskHandler}
-          onRestoreTask={restoreTaskHandler}
-          onDeletePermanently={permanentDeleteHandler}
-        />
-      </Main>
+      <Main />
     </>
   );
 }
--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,3 +1,83 @@
-export default function Main({ children }) {
-  return <main className="bg-[#F5F5F5] relative px-7 pb-1">{children}</main>;
+import { useState } from "react";
+import { INITIAL_TASKS } from "../util/INITIAL_TASKS";
+import RoutesComponent from "./RoutesComponent.jsx";
+
+export default function Main() {
+  const [tasks, setTasks] = useState(
+    JSON.parse(localStorage.getItem("tasks")) || INITIAL_TASKS
+  );
+  const [index, setIndex] = useState(
+    JSON.parse(localStorage.getItem("index")) || 0
+  );
+  const [bin, setBin] = useState(JSON.parse(localStorage.getItem("bin")) || []);
+
+  function addTaskHandler(task) {
+    setTasks((prevTasks) => {
+      localStorage.setItem("tasks", JSON.stringify([...prevTasks, task]));
+      return [...prevTasks, task];
+    });
+  }
+
+  function updateTaskHandler(updatedTask) {
+    setTasks((prevTasks) => {
+      const tasks = [...prevTasks];
+      tasks[index] = { ...updatedTask };
+      localStorage.setItem("tasks", JSON.stringify(tasks));
+      return tasks;
+    });
+  }
+
+  function deleteTaskHandler(task) {
+    setBin((prevBin) => {
+      localStorage.setItem("bin", JSON.stringify([...prevBin, task]));
+      return [...prevBin, task];
+    });
+
+    setTasks((prevTasks) => {
+      const updatedTasks = prevTasks.filter(
+        (prevTask) => prevTask.id !== task.id
+      );
+      localStorage.setItem("tasks", JSON.stringify(updatedTasks));
+      return updatedTasks;
+    });
+  }
+
+  function permanentDeleteHandler(task) {
+    setBin((prevBin) => {
+      const updatedBin = prevBin.filter((binItem) => binItem.id !== task.id);
+      localStorage.setItem("bin", JSON.stringify(updatedBin));
+      return updatedBin;
+    });
+  }
+
+  function restoreTaskHandler(task) {
+    setTasks((prevTasks) => {
+      localStorage.setItem("bin", JSON.stringify([...prevTasks, task]));
+      return [...prevTasks, task];
+    });
+
+    setBin((prevBin) => {
+      const updatedBin = prevBin.filter((binItem) => binItem.id !== task.id);
+      localStorage.setItem("bin", JSON.stringify(updatedBin));
+      return updatedBin;
+    });
+  }
+
+  function changeIndexHandler(index) {
+    localStorage.setItem("index", JSON.stringify(index));
+    setIndex(index);
+  }
+  return <main className="bg-[#F5F5F5] relative px-7 pb-1">
+        <RoutesComponent
+          task={tasks[index]}
+          tasks={tasks}
+          bin={bin}
+          onChangeIndex={changeIndexHandler}
+          onAddTask={addTaskHandler}
+          onDeleteTask={deleteTaskHandler}
+          onUpdateTask={updateTaskHandler}
+          onRestoreTask={restoreTaskHandler}
+          onDeletePermanently={permanentDeleteHandler}
+        />
+  </main>;
 }
