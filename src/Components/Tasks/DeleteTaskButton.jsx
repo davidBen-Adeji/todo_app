0804c8c3f8fe51aa2fe360026b34ff3e67c202ce
david ben-adeji <benadejidavid@gmail.com>
2023-11-18 22:18:24 +0100

    updated ViewTask.jsx... nothing broken yet

--- src/Components/Tasks/DeleteTaskButton.jsx
+++ src/Components/Tasks/DeleteTaskButton.jsx
@@ -1,6 +1,6 @@
 import BinImg from '../../assets/svg/bin.svg'
 
-export default function DeleteButton({ onDeleteTask }) {
+export default function DeleteTaskButton({ onDeleteTask }) {
   return (
     <button onClick={onDeleteTask} type="submit">
       <img className="w-6 h-6" src={BinImg} alt="bin" />
