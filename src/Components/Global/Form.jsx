2e50becd4c3e2afa2457941f37e904540910e040
david ben-adeji <benadejidavid@gmail.com>
2023-11-26 03:56:48 +0100

    updated design

--- src/Components/Global/Form.jsx
+++ src/Components/Global/Form.jsx
@@ -19,7 +19,7 @@ export default function Form({
     >
       <div className="flex items-end mt-10">
         <input
-          className=" bg-white w-[80%] py-2 px-3 capitalize outline-none rounded-full"
+          className=" bg-white w-[75%] py-2 px-3 capitalize outline-none rounded-full"
           type="text"
           value={taskItem}
           placeholder="Add items here"
@@ -32,7 +32,7 @@ export default function Form({
             )
           }
         />
-        <button className="absolute bg-gradient-to-br from-jade-400 to-jade-600 hover:from-jade-500 hover:to-jade-700 h-[100%] w-[20%] right-0 flex justify-center items-center rounded-full" type="submit">
+        <button className="absolute bg-gradient-to-br from-jade-400 to-jade-600 hover:from-jade-500 hover:to-jade-700 h-[100%] w-[25%] lappy:w-[20%] right-0 flex justify-center items-center rounded-full" type="submit">
           <img src={AddImg} className="w-[50%] h-[50%]" alt="add item" />
         </button>
       </div>
