7e60abff60390337d1d3b22afff29d6e46b67070
david ben-adeji <benadejidavid@gmail.com>
2023-12-05 11:57:53 +0100

    updated TodoApp.jsx

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,136 +1,32 @@
-import { useState } from "react";
-import { INITIAL_TASKS } from "../util/INITIAL_TASKS";
 import RoutesComponent from "./RoutesComponent.jsx";
 
-export default function Main({ onUpdateUndoneTasksLength, onUpdateBinLength }) {
-  const [tasks, setTasks] = useState(
-    JSON.parse(localStorage.getItem("tasks")) || INITIAL_TASKS
-  );
-  const [checkedState, setCheckedState] = useState(
-    JSON.parse(localStorage.getItem("tasksCheckedState")) ||
-      new Array(tasks.length).fill(false)
-  );
-  const [index, setIndex] = useState(
-    JSON.parse(localStorage.getItem("index")) || 0
-  );
-  const [bin, setBin] = useState(JSON.parse(localStorage.getItem("bin")) || []);
-  let undoneTasksLength = 0;
-
-  for (const state of checkedState) {
-    if (!state) {
-      undoneTasksLength++;
-    }
-  }
-
-
-
-  onUpdateUndoneTasksLength(undoneTasksLength);
-  onUpdateBinLength(bin.length);
-
-  function toggleCheckBoxHandler(index) {
-    setCheckedState((prevState) => {
-      const newState = [...prevState];
-      newState[index] = !newState[index];
-      localStorage.setItem("tasksCheckedState", JSON.stringify(newState));
-      return newState;
-    });
-  }
-
-  function addTaskHandler(task) {
-    setTasks((prevTasks) => {
-      localStorage.setItem("tasks", JSON.stringify([task, ...prevTasks]));
-      return [task, ...prevTasks];
-    });
-    setCheckedState((prevState) => {
-      localStorage.setItem(
-        "tasksCheckedState",
-        JSON.stringify([false, ...prevState])
-      );
-      return [false, ...prevState];
-    });
-    localStorage.removeItem("newId");
-  }
-
-  function updateTaskHandler(updatedTask) {
-    setTasks((prevTasks) => {
-      const tasks = [...prevTasks];
-      tasks[index] = { ...updatedTask };
-      localStorage.setItem("tasks", JSON.stringify(tasks));
-      return tasks;
-    });
-  }
-
-  function deleteTaskHandler(task, index) {
-    setBin((prevBin) => {
-      localStorage.setItem("bin", JSON.stringify([task, ...prevBin]));
-      return [task, ...prevBin];
-    });
-
-    setTasks((prevTasks) => {
-      const updatedTasks = [...prevTasks];
-      updatedTasks.splice(index, 1);
-      localStorage.setItem("tasks", JSON.stringify(updatedTasks));
-      return updatedTasks;
-    });
-
-    setCheckedState((prevState) => {
-      const newState = [...prevState];
-      newState.splice(index, 1);
-      localStorage.setItem("tasksCheckedState", JSON.stringify(newState));
-      return newState;
-    });
-  }
-
-  function permanentDeleteHandler(task) {
-    localStorage.removeItem(`${task.id}_items`);
-    localStorage.removeItem(`${task.id}_checkedState`);
-    setBin((prevBin) => {
-      const updatedBin = prevBin.filter((item) => item.id !== task.id);
-      localStorage.setItem("bin", JSON.stringify(updatedBin));
-      return updatedBin;
-    });
-  }
-
-  function restoreTaskHandler(task, index) {
-    setTasks((prevTasks) => {
-      localStorage.setItem("tasks", JSON.stringify([task, ...prevTasks]));
-      return [task, ...prevTasks];
-    });
-
-    setCheckedState((prevState) => {
-      localStorage.setItem(
-        "tasksCheckedState",
-        JSON.stringify([false, ...prevState])
-      );
-      return [false, ...prevState];
-    });
-
-    setBin((prevBin) => {
-      const updatedBin = [...prevBin];
-      updatedBin.splice(index, 1);
-      localStorage.setItem("bin", JSON.stringify(updatedBin));
-      return updatedBin;
-    });
-  }
-
-  function changeIndexHandler(index) {
-    localStorage.setItem("index", JSON.stringify(index));
-    setIndex(index);
-  }
+export default function Main({
+  task,
+  tasks,
+  checkedState,
+  bin,
+  onChangeIndex,
+  onAddTask,
+  onDeleteTask,
+  onUpdateTask,
+  onRestoreTask,
+  onDeletePermanently,
+  onToggleCheckBox,
+}) {
   return (
     <main className="bg-white px-7 pb-1 overflow-y-scroll">
       <RoutesComponent
-        task={tasks[index]}
+        task={task}
         tasks={tasks}
         checkedState={checkedState}
         bin={bin}
-        onChangeIndex={changeIndexHandler}
-        onAddTask={addTaskHandler}
-        onDeleteTask={deleteTaskHandler}
-        onUpdateTask={updateTaskHandler}
-        onRestoreTask={restoreTaskHandler}
-        onDeletePermanently={permanentDeleteHandler}
-        onToggleCheckBox={toggleCheckBoxHandler}
+        onChangeIndex={onChangeIndex}
+        onAddTask={onAddTask}
+        onDeleteTask={onDeleteTask}
+        onUpdateTask={onUpdateTask}
+        onRestoreTask={onRestoreTask}
+        onDeletePermanently={onDeletePermanently}
+        onToggleCheckBox={onToggleCheckBox}
       />
     </main>
   );
--- src/Components/TodoApp.jsx
+++ src/Components/TodoApp.jsx
@@ -1,35 +1,153 @@
 import { useState } from "react";
+import { INITIAL_TASKS } from "../util/INITIAL_TASKS";
 import Header from "./Header/Header.jsx";
 import Main from "./Main.jsx";
 
 export default function TodoApp() {
-  const [undoneTasksLength, setUndoneTasksLength] = useState(
-    JSON.parse(localStorage.getItem("undoneTasksLength")) || 0
+  const [tasks, setTasks] = useState(
+    JSON.parse(localStorage.getItem("tasks")) || INITIAL_TASKS
   );
-  const [binLength, setBinLength] = useState(
-    JSON.parse(localStorage.getItem("binLength")) || 0
+  const [checkedState, setCheckedState] = useState(
+    JSON.parse(localStorage.getItem("tasksCheckedState")) ||
+      new Array(tasks.length).fill(false)
   );
+  const [index, setIndex] = useState(
+    JSON.parse(localStorage.getItem("index")) || 0
+  );
+  const [bin, setBin] = useState(JSON.parse(localStorage.getItem("bin")) || []);
 
-  function updateUndoneTasksLengthHandler(lengthValue) {
-    if (undoneTasksLength === lengthValue) return;
-
-    localStorage.setItem("undoneTasksLength", JSON.stringify(lengthValue));
-    setUndoneTasksLength(lengthValue);
+  let undoneTasksLength = 0;
+  for (const state of checkedState) {
+    if (!state) undoneTasksLength++;
   }
 
-  function updateBinLengthHandler(lengthValue) {
-    if (binLength === lengthValue) return;
+  const idCheckedState = JSON.parse(
+    localStorage.getItem(`${tasks[index].id}_checkedState`)
+  );
+  let isAllTaskItemsChecked = false;
 
-    localStorage.setItem("binLength", JSON.stringify(lengthValue));
-    setBinLength(lengthValue);
+  if (idCheckedState) {
+    for (const state of idCheckedState) {
+      if (state) isAllTaskItemsChecked = true;
+      else isAllTaskItemsChecked = false;
+    }
+  }
+
+  // if (isAllTaskItemsChecked) {
+  //   setCheckedState((prevState) => {
+  //     const newState = [...prevState];
+  //     newState[index] = !newState[index];
+  //     localStorage.setItem("tasksCheckedState", JSON.stringify(newState));
+  //     return newState;
+  //   });
+  // }
+
+  function toggleCheckBoxHandler(index) {
+    setCheckedState((prevState) => {
+      const newState = [...prevState];
+      newState[index] = !newState[index];
+      localStorage.setItem("tasksCheckedState", JSON.stringify(newState));
+      return newState;
+    });
+  }
+
+  function addTaskHandler(task) {
+    setTasks((prevTasks) => {
+      localStorage.setItem("tasks", JSON.stringify([task, ...prevTasks]));
+      return [task, ...prevTasks];
+    });
+    setCheckedState((prevState) => {
+      localStorage.setItem(
+        "tasksCheckedState",
+        JSON.stringify([false, ...prevState])
+      );
+      return [false, ...prevState];
+    });
+    localStorage.removeItem("newId");
+  }
+
+  function updateTaskHandler(updatedTask) {
+    setTasks((prevTasks) => {
+      const tasks = [...prevTasks];
+      tasks[index] = { ...updatedTask };
+      localStorage.setItem("tasks", JSON.stringify(tasks));
+      return tasks;
+    });
+  }
+
+  function deleteTaskHandler(task, index) {
+    setBin((prevBin) => {
+      localStorage.setItem("bin", JSON.stringify([task, ...prevBin]));
+      return [task, ...prevBin];
+    });
+
+    setTasks((prevTasks) => {
+      const updatedTasks = [...prevTasks];
+      updatedTasks.splice(index, 1);
+      localStorage.setItem("tasks", JSON.stringify(updatedTasks));
+      return updatedTasks;
+    });
+
+    setCheckedState((prevState) => {
+      const newState = [...prevState];
+      newState.splice(index, 1);
+      localStorage.setItem("tasksCheckedState", JSON.stringify(newState));
+      return newState;
+    });
+  }
+
+  function permanentDeleteHandler(task) {
+    localStorage.removeItem(`${task.id}_items`);
+    localStorage.removeItem(`${task.id}_checkedState`);
+    setBin((prevBin) => {
+      const updatedBin = prevBin.filter((item) => item.id !== task.id);
+      localStorage.setItem("bin", JSON.stringify(updatedBin));
+      return updatedBin;
+    });
+  }
+
+  function restoreTaskHandler(task, index) {
+    setTasks((prevTasks) => {
+      localStorage.setItem("tasks", JSON.stringify([task, ...prevTasks]));
+      return [task, ...prevTasks];
+    });
+
+    setCheckedState((prevState) => {
+      localStorage.setItem(
+        "tasksCheckedState",
+        JSON.stringify([false, ...prevState])
+      );
+      return [false, ...prevState];
+    });
+
+    setBin((prevBin) => {
+      const updatedBin = [...prevBin];
+      updatedBin.splice(index, 1);
+      localStorage.setItem("bin", JSON.stringify(updatedBin));
+      return updatedBin;
+    });
+  }
+
+  function changeIndexHandler(index) {
+    localStorage.setItem("index", JSON.stringify(index));
+    setIndex(index);
   }
 
   return (
     <>
-      <Header undoneTasksLength={undoneTasksLength} binLength={binLength} />
+      <Header undoneTasksLength={undoneTasksLength} binLength={bin.length} />
       <Main
-        onUpdateUndoneTasksLength={updateUndoneTasksLengthHandler}
-        onUpdateBinLength={updateBinLengthHandler}
+        task={tasks[index]}
+        tasks={tasks}
+        checkedState={checkedState}
+        bin={bin}
+        onChangeIndex={changeIndexHandler}
+        onAddTask={addTaskHandler}
+        onDeleteTask={deleteTaskHandler}
+        onUpdateTask={updateTaskHandler}
+        onRestoreTask={restoreTaskHandler}
+        onDeletePermanently={permanentDeleteHandler}
+        onToggleCheckBox={toggleCheckBoxHandler}
       />
     </>
   );
