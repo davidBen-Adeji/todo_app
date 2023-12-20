6c7ba265f25bb7c9e1d50909d7c6e99182318411
david ben-adeji <benadejidavid@gmail.com>
2023-12-20 20:57:20 +0100

    reinstalled tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {
-      screens: {
-        // 900.98px
-        lappy: "56.31125rem",
-      },
-      colors: {
-        white: "#ffffff",
-        jade: {
-          50: "#ebfef4",
-          100: "#d0fbe3",
-          200: "#a4f6cc",
-          300: "#6aebb1",
-          400: "#2fd891",
-          500: "#0abf79",
-          600: "#00a86b",
-          700: "#007c52",
-          800: "#036242",
-          900: "#045038",
-          950: "#012d21",
-        },
-      },
-    },
+    extend: {},
   },
   plugins: [],
-};
+}
